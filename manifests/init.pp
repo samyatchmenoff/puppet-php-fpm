75d5206f50d49d63d7ee8d75752d3b4c9b5d8f7e
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-03-17 18:49:33 +0100

    add parameters for general php-fpm configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,18 @@
-class php-fpm {
+class php-fpm(
+  $pid = '/var/run/php5-fpm.pid',
+  $error_log = '/var/log/php5-fpm.log',
+  $syslog_facility = 'daemon',
+  $syslog_ident = 'php-fpm',
+  $log_level = 'notice',
+  $emergency_restart_threshold = 0,
+  $emergency_restart_interval = 0,
+  $process_control_timeout = 0,
+  $process_max = 128,
+  $daemonize = yes,
+  $rlimit_files = 1024,
+  $rlimit_core = 0,
+  $events_mechanism = false,
+){
   anchor { 'php-fpm::begin':
     before => Class['php-fpm::install'],
     notify => Class['php-fpm::service'],
